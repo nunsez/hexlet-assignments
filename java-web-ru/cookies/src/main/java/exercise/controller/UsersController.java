80a0cf472e2b28266d366b53a94079954c78e832
@hexlet/cli <support@hexlet.io>
2024-06-05 12:09:56 +0700

    submit java-web-ru/cookies

--- java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -21,14 +21,16 @@ public class UsersController {
     private static final String TOKEN_NAME = "token";
 
     public static void create(Context ctx) {
-        var firstName = ctx.formParamAsClass("firstName", String.class).get();
-        var lastName = ctx.formParamAsClass("lastName", String.class).get();
-        var email = ctx.formParamAsClass("email", String.class).get();
+        var firstName = StringUtils.capitalize(ctx.formParam("firstName"));
+        var lastName = StringUtils.capitalize(ctx.formParam("lastName"));
+        var email = ctx.formParamAsClass("email", String.class).get().strip().toLowerCase();
         var password = ctx.formParamAsClass("password", String.class).get();
+        var encryptedPassword = Security.encrypt(password);
         var token = Security.generateToken();
 
-        var user = new User(firstName, lastName, email, password, token);
+        var user = new User(firstName, lastName, email, encryptedPassword, token);
         UserRepository.save(user);
+
         ctx.cookie(TOKEN_NAME, token);
         ctx.redirect(NamedRoutes.userPath(user.getId()));
     }
@@ -38,7 +40,7 @@ public class UsersController {
         var user = UserRepository.find(id).orElseThrow(() -> new NotFoundResponse("Пользователь не найден"));
         var token = ctx.cookie(TOKEN_NAME);
 
-        if (StringUtils.equals(user.getToken(), token)) {
+        if (token != null && StringUtils.equals(user.getToken(), token)) {
             var page = new UserPage(user);
             ctx.render("users/show.jte", model("page", page));
         } else {
