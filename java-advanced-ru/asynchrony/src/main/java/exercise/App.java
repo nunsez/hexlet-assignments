b288ac4e5bc30434a7abe4366d472099d908ff71
@hexlet/cli <support@hexlet.io>
2024-06-30 19:33:40 +0700

    submit java-advanced-ru/asynchrony

--- java-advanced-ru/asynchrony/src/main/java/exercise/App.java
+++ java-advanced-ru/asynchrony/src/main/java/exercise/App.java
@@ -1,57 +1,87 @@
 package exercise;
 
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.Files;
-import java.io.IOException;
+import java.io.File;
 
 class App {
 
     // BEGIN
+    private static Path getFullPath(String filePath) {
+        return Paths.get(filePath).toAbsolutePath().normalize();
+    }
+
     public static CompletableFuture<String> unionFiles(String src1, String src2, String dest) {
-        var feature1 = CompletableFuture.supplyAsync(() -> {
+        var content1 = CompletableFuture.supplyAsync(() -> {
             try {
-                return Files.readString(Path.of(src1));
+                return Files.readString(getFullPath(src1));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
 
-        var feature2 = CompletableFuture.supplyAsync(() -> {
+        var content2 = CompletableFuture.supplyAsync(() -> {
             try {
-                return Files.readString(Path.of(src2));
-            } catch (IOException e) {
+                return Files.readString(getFullPath(src2));
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
 
-        var result = feature1
-            .thenCombine(feature2, (file1, file2) -> {
-                var content = file1 + file2;
+        return content1.thenCombine(content2, (cont1, cont2) -> {
+            var union = cont1 + cont2;
+            try {
+                Files.writeString(getFullPath(dest), union, StandardOpenOption.CREATE);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return union;
 
-                try {
-                    Files.writeString(Path.of(dest), content);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+        }).exceptionally(ex -> {
+            System.out.println("Oops! We have an exception - " + ex.getMessage());
+            return null;
+        });
+    }
 
-                return content;
-            }).exceptionally(e -> {
-                System.out.println(e.getMessage());
-                return null;
-            });
+    public static CompletableFuture<Long> getDirectorySize(String path) {
+        var directory = new File(path);
 
-        return result;
+        if (!directory.isDirectory()) {
+            return CompletableFuture.completedFuture(0L);
+        }
+
+        var files = directory.listFiles();
+
+        var fileSizes = Arrays.stream(files)
+            .filter(File::isFile)
+            .map(file -> CompletableFuture.supplyAsync(() -> file.length()))
+            .toArray(CompletableFuture[]::new);
+
+        return CompletableFuture.allOf(fileSizes)
+            .thenApply(v -> Arrays
+                .stream(fileSizes)
+                .mapToLong(x -> (long) x.join())
+                .sum()
+            );
     }
     // END
 
     public static void main(String[] args) throws Exception {
         // BEGIN
-        var file1 = "src/main/resources/file1.txt";
-        var file2 = "src/main/resources/file2.txt";
-        var file3 = "src/main/resources/file3.txt";
-        unionFiles(file1, file2, file3).get();
+        var result = unionFiles(
+            "src/main/resources/file1.txt",
+            "src/main/resources/file2.txt",
+            "src/main/resources/file3.txt"
+        );
+        CompletableFuture<Long> size = getDirectorySize("src/main/resources");
+        result.get();
+        System.out.println("done!");
+        System.out.println(size.get());
         // END
     }
 }
